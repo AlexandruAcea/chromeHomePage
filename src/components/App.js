612ac3e1e5923dc95550117fc329a04330536370
AlexandruAcea <alex.acea98@gmail.com>
2019-03-26 11:40:30 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import "../css/App.css";
+import React, { Component } from "react";
+import { BrowserRouter, Router } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
